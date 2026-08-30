5703ec775fe7ae90bfa3970617b9b8b2b16f868c
Aysel-Bayramova <ayselbayramova0902@gmail.com>
2026-08-30 15:51:19 +0000

    Task 4: Add StudentClass with interfaces for constructor and class instance

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -19,4 +19,33 @@ const printTeacher: printTeacherFunction = (firstName: string, lastName: string)
   return `${firstName.charAt(0)}. ${lastName}`;
 };
 
-console.log(printTeacher("John", "Doe"));
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
+}
+
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = new StudentClass("Aysel", "Bayramova");
+console.log(student.displayName());
+console.log(student.workOnHomework());
